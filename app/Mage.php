fbd85edeeee6ef782819ef5d5d695e48012e8e46
dimav <dimav@ee4975a1-dc2a-0410-ac7a-f60e980b784d>
2011-10-19 16:51:53 +0000

    Added lost changes
    git-svn-id: http://svn.magentocommerce.com/source/branches/1.6@127755 ee4975a1-dc2a-0410-ac7a-f60e980b784d

--- app/Mage.php
+++ app/Mage.php
@@ -152,17 +152,10 @@ final class Mage
         return array(
             'major'     => '1',
             'minor'     => '6',
-<<<<<<< .working
-            'revision'  => '0',
-            'patch'     => '0',
-            'stability' => '',
-            'number'    => '',
-=======
             'revision'  => '1',
             'patch'     => '0',
             'stability' => '',
             'number'    => '',
->>>>>>> .merge-right.r127595
         );
     }
 
